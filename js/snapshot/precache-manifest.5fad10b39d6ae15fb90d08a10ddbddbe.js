a0b5828a2379675175386e61da49244cd5cf0eeb
mahdi mehrabi <mahdimehrabi17@gmail.com>
2020-08-12 04:19:19 +0430

    added snapshot project

---
+++ js/snapshot/precache-manifest.5fad10b39d6ae15fb90d08a10ddbddbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "514b78571cd1d26ed4f99483dd378e54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3a816968ce7d57a7d96",
+    "url": "/static/js/2.f83ce6f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f83ce6f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ad512d419d9421c41a",
+    "url": "/static/js/main.12bc95a4.chunk.js"
+  },
+  {
+    "revision": "4a42fdcbdfd3cf55fea3",
+    "url": "/static/js/runtime-main.1c47a842.js"
+  }
+]);
